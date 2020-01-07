b90cfdb414cacd736d7b025d51ab69f3eb98178f
Koitrin KOFFI <koitrinstephanedesire@gmail.com>
2020-01-07 17:20:03 +0100

    first commit

---
+++ src/main/java/fr/istic/master1/sir/App.java
@@ -0,0 +1,34 @@
+package fr.istic.master1.sir;
+
+import java.awt.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    /**
+     * Point d'entré de l'app
+     * @param args j'ai jamais su à quoi il sert celui là
+     */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public void superMethod() {
+        String test = "lol";
+        while (true) {
+            Color[] colors = {Color.BLACK, Color.WHITE, Color.RED, Color.BLUE, Color.GREEN, Color.GRAY, Color.YELLOW};
+        }
+    }
+
+    public void superMethod2() {
+        String test = "lol";
+        while (true) {
+            Color[] colors = {Color.BLACK, Color.WHITE, Color.RED, Color.BLUE, Color.GREEN, Color.GRAY, Color.YELLOW};
+            System.out.println(colors[0]);
+        }
+    }
+}
